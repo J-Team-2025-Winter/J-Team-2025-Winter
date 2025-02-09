90de0efb09851b5b64e53e7bf36bde90eef70e77
hiyo@49 <k77mu04@gmail.com>
2025-02-09 22:33:25 +0900

    [update]データベースTable名修正

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -7,25 +7,25 @@ CREATE DATABASE chatapp;
 USE chatapp
 GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
 
-CREATE TABLE users (
-    CustomerID VARCHAR(255) PRIMARY KEY,
-    CustomerName VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) UNIQUE NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    Phone VARCHAR(11) NOT NULL,
-    Gender ENUM('Male', 'Female', 'Other') NOT NULL
+CREATE TABLE customers (
+    customer_id VARCHAR(255) PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(11) NOT NULL,
+    gender ENUM('Male', 'Female', 'Other') NOT NULL
 );
 
 CREATE TABLE stylists (
-    StylistID VARCHAR(255) PRIMARY KEY,
-    StoreID INT,
-    StylistName VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) UNIQUE NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    Phone VARCHAR(11) NOT NULL,
-    Gender ENUM('Male', 'Female', 'Other') NOT NULL,
-    ProfilePictureURL VARCHAR(255),
-    Comment VARCHAR(255)
+    stylist_id VARCHAR(255) PRIMARY KEY,
+    store_id INT,
+    stylist_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(11) NOT NULL,
+    gender ENUM('Male', 'Female', 'Other') NOT NULL,
+    profile_picture_url VARCHAR(255),
+    comment VARCHAR(255)
 );
 
 -- CREATE TABLE channels (
@@ -46,7 +46,7 @@ CREATE TABLE stylists (
 --     FOREIGN KEY (cid) REFERENCES channels(id) ON DELETE CASCADE
 -- );
 
-INSERT INTO users(CustomerID, CustomerName, Email, Password, Phone, Gender) VALUES('00000000-0000-0000-0000-000000000000','Customer Zero','customerzero@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578', '00011112222', 'Male');
-INSERT INTO stylists(StylistID, StoreID, StylistName, Email, Password, Phone, Gender, ProfilePictureURL, Comment) VALUES('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', '1', 'Stylist Zero','stylistzero@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578', '33344445555', 'Male', 'profile-picture', 'hasami no koto nara omakase kudasai');
+INSERT INTO customers(customer_id, customer_name, email, password, phone, gender) VALUES('00000000-0000-0000-0000-000000000000','Customer Zero','customerzero@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578', '00011112222', 'Male');
+INSERT INTO stylists(stylist_id, store_id, stylist_name, email, password, phone, gender, profile_picture_url, comment) VALUES('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', '1', 'Stylist Zero','stylistzero@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578', '33344445555', 'Male', 'profile-picture', 'hasami no koto nara omakase kudasai');
 -- INSERT INTO channels(id, uid, name, abstract) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
 -- INSERT INTO messages(id, uid, cid, message) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
